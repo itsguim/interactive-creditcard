8be4d784a17100648bccca783ee59a5c59ebdb86
KaimuraDev <italoguimaraes13@hotmail.com>
2022-10-14 15:18:49 -0300

    ✨add: remaining input interactions

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
-const cardNumberInput = document.querySelector('#cardnumber')
-const cardNumberField = document.querySelector('.card__number')
+// Inputs
+const nameInput = document.querySelector('#cardname')
+const numberInput = document.querySelector('#cardnumber')
+const monthInput = document.querySelector('#month')
+const yearInput = document.querySelector('#year')
+const cvcInput = document.querySelector('#cardcvc')
 
-function passCardNumber(evt) {
-  let getInputValue = evt.target.value
+function initCardInteraction() {
 
-  if (!getInputValue == '') {
-    getInputValue = getInputValue.replace(/[-.]+/g, ' ')
-    cardNumberField.innerText = getInputValue
-  } else if (getInputValue == '') {
-    cardNumberField.innerText = `0000 0000 0000 0000`
+  // Card field selectors / Seletores do cartão
+  const cardName = document.querySelector('.card__name')
+  const cardNumber = document.querySelector('.card__number')
+  const cardMonth = document.querySelector('.js__mm')
+  const cardYear = document.querySelector('.js__yy')
+  const cardCvc = document.querySelector('.card__cvc')
+
+  function passCardName(e) {
+    let inputValue = e.target.value
+    cardName.innerText = inputValue
+    if (!inputValue) {
+      cardName.innerText = 'Erick Cabral'
+    }
   }
 
-}
+  function passCardNumber(e) {
+    let inputValue = e.target.value
+    if (inputValue) {
+      inputValue = inputValue.replace(/[-.]+/g, ' ')
+      cardNumber.innerText = inputValue
+    } else if (!inputValue) {
+      cardNumber.innerText = `0000 0000 0000 0000`
+    }
+  }
 
-cardNumberInput.addEventListener('keyup', passCardNumber)
+  function passCardDate() {
+    let month = monthInput.value
+    let year = yearInput.value
+    cardMonth.innerText = month
+    cardYear.innerHTML = year
+    if (!month && !year) {
+      cardMonth.innerText = '00'
+      cardYear.innerText = '00'
+    }
+  }
+
+  function passCvc(e) {
+    let inputValue = e.target.value
+    cardCvc.innerText = inputValue
+    if (!inputValue) { cardCvc.innerText = '000' }
+  }
+
+  // Card interaction events / Listeners pra interação com cartão 
+  nameInput.addEventListener('keyup', passCardName)
+  numberInput.addEventListener('keyup', passCardNumber)
+  monthInput.addEventListener('keyup', passCardDate)
+  yearInput.addEventListener('keyup', passCardDate)
+  cvcInput.addEventListener('keyup', passCvc)
+}
+initCardInteraction()
+
+
+
+
 
